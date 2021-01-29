debecd70bec9a77cd41c10e647bc56596717dcc8
kerno.yu <kerno.yu@oracle.com>
2021-01-29 23:01:17 +0800

    tt

---
+++ src/test/java/com/example/demo/jvm/TestGc.java
@@ -0,0 +1,14 @@
+package com.example.demo.jvm;
+
+
+/**
+ * -Xms10m -Xmx10m
+ */
+
+public class TestJVM {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("hello1");
+    }
+
+}
---
+++ src/test/java/com/example/demo/jvm/TestJVM2.java
@@ -0,0 +1,15 @@
+package com.example.demo.jvm;
+
+
+/**
+ * -Xms=10m -Xmx=10m
+ */
+
+public class TestJVM {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("hello");
+        Thread.sleep(1000000l);
+    }
+
+}
